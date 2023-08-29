6d72e030bf2ade88589b9d50428b86b1b1c892b9
Rajat Choudhary <rajat.cloudwapp@gmail.com>
2023-08-29 16:13:39 +0530

    add my frontend Code

--- gitTest.js
+++
@@ -1,8 +0,0 @@
-console.log("okkkk");
-
-function test(a,b){
-    console.log(a+b)
-}
-let a=10;
-let b=20;
-test(a,b)
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const userController= require('../controllers/userControllers')
-
-router.get('/getdata',userController.getAllusers)
-router.post('/setData',userController.setAllusers)
-
-module.exports=router
